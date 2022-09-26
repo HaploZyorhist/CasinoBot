a4c7ad044463eef6eb70b5a642281e7fb2ceb681
David John <djohn@unionwireless.com>
2022-09-26 21:47:40 +0200

    Add project files.

---
+++ CasinoBot/Program.cs
@@ -0,0 +1,57 @@
+﻿using CasinoBot.Services;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Interactivity;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CasinoBot
+{
+    public class Program
+    {
+        private DiscordSocketClient _client;
+
+        public static Task Main(string[] args) => new Program().MainAsync();
+
+        public async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+
+            var services = ConfigureServices();
+
+            await services.GetRequiredService<CommandHandlerService>().InitializeAsync(services);
+
+            var token = Environment.GetEnvironmentVariable("token");
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+
+        private IServiceProvider ConfigureServices()
+        {
+            return new ServiceCollection()
+                // Base
+                .AddSingleton(_client)
+                .AddSingleton<CommandService>()
+                .AddSingleton<InteractivityService>()
+                .AddSingleton(new InteractivityConfig { DefaultTimeout = TimeSpan.FromSeconds(15) })
+
+                // Custom Services
+                .AddSingleton<CommandHandlerService>()
+
+                // Database
+                //.AddDbContext<LoLBotContext>(options =>
+                //options.UseSqlServer(Environment.GetEnvironmentVariable("DBConnection"),
+                //sqlServerOptionsAction: sqlOptions =>
+                //{
+                // sqlOptions.EnableRetryOnFailure();
+                //}
+                //),
+                //ServiceLifetime.Transient)
+
+                .BuildServiceProvider();
+        }
+    }
+}
---
+++ CasinoBot/Services/CommandHandlerService.cs
@@ -0,0 +1,91 @@
+﻿using System.Reflection;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace CasinoBot.Services
+{
+    /// <summary>
+    /// service class for handling commands
+    /// </summary>
+    public class CommandHandlerService
+    {
+
+        #region Fields
+
+        private readonly CommandService _commands;
+        private readonly DiscordSocketClient _client;
+        private IServiceProvider _provider;
+
+        #endregion
+
+        #region CTOR
+
+        /// <summary>
+        /// CTOR for class
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="provider"></param>
+        /// <param name="commands"></param>
+        public CommandHandlerService(DiscordSocketClient client,
+                                     IServiceProvider provider,
+                                     CommandService commands)
+        {
+            _commands = commands;
+            _client = client;
+            _provider = provider;
+
+            _client.MessageReceived += MessageReceived;
+        }
+
+        #endregion
+
+        #region CommandHandler
+
+        /// <summary>
+        /// Task for setting up the class
+        /// </summary>
+        /// <param name="provider"></param>
+        public async Task InitializeAsync(IServiceProvider provider)
+        {
+            _provider = provider;
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _provider);
+        }
+
+        /// <summary>
+        /// Message handler, should manage the commands
+        /// </summary>
+        /// <param name="message"></param>
+        private async Task MessageReceived(SocketMessage message)
+        {
+            var prefix = Environment.GetEnvironmentVariable("prefix");
+
+            if (!(message is SocketUserMessage socketMessage))
+            {
+                return;
+            }
+
+            if (message.Source != Discord.MessageSource.User)
+            {
+                return;
+            }
+
+            var context = new SocketCommandContext(_client, socketMessage);
+
+            if (prefix != "!" && message.Content.ToLower().StartsWith("!prefix"))
+            {
+                prefix = "!";
+                await _commands.ExecuteAsync(context, prefix.Length, _provider);
+            }
+            else if (!(message.ToString().StartsWith(prefix)))
+            {
+                return;
+            }
+            else
+            {
+                await _commands.ExecuteAsync(context, prefix.Length, _provider);
+            }
+        }
+
+        #endregion
+    }
+}
